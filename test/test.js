764b6defa777339348a1c4cfef08031e8bf33a45
edward.xu <edward.xu@qunar.com>
2018-10-29 15:57:54 +0800

    add: add new test case

--- test/test.js
+++ test/test.js
@@ -43,4 +43,9 @@ describe('#numFormatter', function() {
         var result = numFormatter(12345678);
         expect(result).to.equal('12,345,678');
     });
+
+    it('should convert 9 digits', function() {
+        var result = numFormatter(12345678);
+        expect(result).to.equal('123,456,789');
+    });
 });
